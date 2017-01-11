cd428876ff0b57fcdcd7b1e216a08a715134137f
ravi <ravi.kandhadai@epfl.ch>
2017-01-11 18:00:48 +0100

    Proving termination of Viterbi

--- testcases/lazy-datastructures/withOrb/NewViterbi.scala
+++ testcases/lazy-datastructures/withOrb/NewViterbi.scala
@@ -9,7 +9,6 @@ import invariant._
 import collection._
 import math._
 
-// DO NOT remove the commented code, lemmas are hard to come up with :-)
 
 /**
  * Implementation of the Viterbi algorithm
@@ -70,122 +69,128 @@ object Viterbi {
     xstring(i.toInt)
   } ensuring (_ => steps <= 1)
 
-	def deps(j: BigInt, K: BigInt): Boolean = {
-		require(j >= 0 && K >= 0)
-		if(j <= 0) true
-		else columnsCachedfrom(j - 1, K)
-	}
-
-	def columnsCachedfrom(j: BigInt, K: BigInt): Boolean = {
-		require(j >= 0 && K >= 0)
-		columnCached(K, j, K) && {
-		if(j <= 0) true
-		else columnsCachedfrom(j - 1, K)
-		}
-	}
-
-	def columnCached(i: BigInt, j: BigInt, K: BigInt): Boolean = {
-		require(i >= 0 && j >= 0 && K >= i)
-		cached(viterbi(i, j, K)) && {
-		if(i <= 0) true
-		else columnCached(i - 1, j, K)
-		}
+  def deps(j: BigInt, K: BigInt): Boolean = {
+    require(j >= 0 && K >= 0)
+    if (j <= 0) true
+    else columnsCachedfrom(j - 1, K)
   }
 
-	@traceInduct
-  	def columnMono(i: BigInt, j: BigInt, K: BigInt, st1: Set[Fun[BigInt]], st2: Set[Fun[BigInt]]) = {
-    	require(i >= 0 && j >= 0 && K >= i)
-    	(st1.subsetOf(st2) && (columnCached(i, j, K) in st1)) ==> (columnCached(i, j, K) in st2)
-  	} holds
+  def columnsCachedfrom(j: BigInt, K: BigInt): Boolean = {
+    require(j >= 0 && K >= 0)
+    columnCached(K, j, K) && {
+      if (j <= 0) true
+      else columnsCachedfrom(j - 1, K)
+    }
+  }
 
-  	@traceInduct
-	def columnLem(j: BigInt, K: BigInt): Boolean = {
-   		require(j >= 0 && K >= 0)
-   		if(j <= 0) (columnCached(K, j, K)) ==> (columnsCachedfrom(j, K))
-   		else (columnsCachedfrom(j - 1, K) && columnCached(K, j, K)) ==> (columnsCachedfrom(j, K))
-   	} holds
+  def columnCached(i: BigInt, j: BigInt, K: BigInt): Boolean = {
+    require(i >= 0 && j >= 0 && K >= i)
+    cached(viterbi(i, j, K)) && {
+      if (i <= 0) true
+      else columnCached(i - 1, j, K)
+    }
+  }
 
-  // Does this terminate ? this requires a very complex ranking function: what is it ?
-	def cachedLem(l: BigInt, j: BigInt, K: BigInt): Boolean = {
-	 require(j >= 0 && l >= 0 && K >= l)
-	 (if(l == K) true
-	   else if(l == 0) cachedLem(l + 1, j, K)
-	   else cachedLem(l + 1, j, K) && cachedLem(l - 1, j, K)
-	   ) && (columnCached(K, j, K) ==> columnCached(l, j, K))
-	} holds
+  @traceInduct
+  def columnMono(i: BigInt, j: BigInt, K: BigInt, st1: Set[Fun[BigInt]], st2: Set[Fun[BigInt]]) = {
+    require(i >= 0 && j >= 0 && K >= i)
+    (st1.subsetOf(st2) && (columnCached(i, j, K) in st1)) ==> (columnCached(i, j, K) in st2)
+  } holds
 
-	def columnsCachedfromMono(j: BigInt, K: BigInt, st1: Set[Fun[BigInt]], st2: Set[Fun[BigInt]]): Boolean = {
-		require(j >= 0 && K >= 0)
-		(columnMono(K, j, K, st1, st2) && (j <= 0 || columnsCachedfromMono(j - 1, K, st1, st2))) &&
-		((st1.subsetOf(st2) && (columnsCachedfrom(j, K) in st1)) ==> (columnsCachedfrom(j, K) in st2))
-	} holds
+  @traceInduct
+  def columnLem(j: BigInt, K: BigInt): Boolean = {
+    require(j >= 0 && K >= 0)
+    if (j <= 0) (columnCached(K, j, K)) ==> (columnsCachedfrom(j, K))
+    else (columnsCachedfrom(j - 1, K) && columnCached(K, j, K)) ==> (columnsCachedfrom(j, K))
+  } holds
+
+  def cachedLem(l: BigInt, j: BigInt, K: BigInt): Boolean = {
+    require(j >= 0 && l >= 0 && K >= l)
+    // inner function that implements the induction strategy
+    @invisibleBody
+    def cachedLemInduct(l: BigInt, j: BigInt, varK: BigInt, K: BigInt): Boolean = {
+      require(j >= 0 && l >= 0 && varK >= l && K >= varK)
+      (if (l == varK) true
+      else cachedLemInduct(l, j, varK - 1, K)) &&
+        (columnCached(varK, j, K) ==> columnCached(l, j, K))
+    } holds
+
+    cachedLemInduct(l, j, K, K) && (columnCached(K, j, K) ==> columnCached(l, j, K))
+  }
+
+  def columnsCachedfromMono(j: BigInt, K: BigInt, st1: Set[Fun[BigInt]], st2: Set[Fun[BigInt]]): Boolean = {
+    require(j >= 0 && K >= 0)
+    (columnMono(K, j, K, st1, st2) && (j <= 0 || columnsCachedfromMono(j - 1, K, st1, st2))) &&
+      ((st1.subsetOf(st2) && (columnsCachedfrom(j, K) in st1)) ==> (columnsCachedfrom(j, K) in st2))
+  } holds
 
   @invstate
   def fillEntry(l: BigInt, i: BigInt, j: BigInt, K: BigInt): BigInt = {
+    decreases((abs(j), abs(K - l)))
     require(i >= 0 && j >= 1 && l >= 0 && K >= l && K >= i && deps(j, K) && cachedLem(l, j - 1, K))
     val a1 = viterbi(l, j - 1, K) + A(l, i) + B(i, Y(j))
-    if(l == K) a1
+    if (l == K) a1
     else {
-      val a2 = fillEntry(l + 1, i, j, K) // have a look at the algo again @Sumith
-      if(a1 > a2) a1 else a2
+      val a2 = fillEntry(l + 1, i, j, K)
+      if (a1 > a2) a1 else a2
     }
-  } ensuring(steps <= ? * (K - l) + ?)
+  } ensuring (steps <= ? * (K - l) + ?)
 
   @invstate
   @memoize
   def viterbi(i: BigInt, j: BigInt, K: BigInt): BigInt = {
     require(i >= 0 && j >= 0 && K >= i && deps(j, K))
-    if(j == 0) {
+    if (j == 0) {
       C(i) + B(i, Y(0))
     } else {
       fillEntry(0, i, j, K)
     }
-  } ensuring(steps <= ? * K + ?)
+  } ensuring (steps <= ? * K + ?)
 
   def invoke(i: BigInt, j: BigInt, K: BigInt): BigInt = {
     require(i >= 0 && j >= 0 && K >= i && deps(j, K) && (i == 0 || i > 0 && columnCached(i - 1, j, K)))
     viterbi(i, j, K)
-  } ensuring(res => {
+  } ensuring (res => {
     val in = inSt[BigInt]
     val out = outSt[BigInt]
     (j == 0 || columnsCachedfromMono(j - 1, K, in, out)) && columnsCachedfromMono(j, K, in, out) &&
-    (i == 0 || columnMono(i - 1, j, K, in, out)) && columnCached(i, j, K) &&
-    steps <= ? * K + ?
+      (i == 0 || columnMono(i - 1, j, K, in, out)) && columnCached(i, j, K) &&
+      steps <= ? * K + ?
   })
 
   def fillColumn(i: BigInt, j: BigInt, K: BigInt): List[BigInt] = {
+    decreases(abs(K - i))
     require(i >= 0 && j >= 0 && K >= i && deps(j, K) && (i == 0 || i > 0 && columnCached(i - 1, j, K)))
-    if(i == K) {
+    if (i == K) {
       val x = invoke(i, j, K)
       Cons(x, Nil[BigInt]())
-    }
-    else {
+    } else {
       val x = invoke(i, j, K)
       val tail = fillColumn(i + 1, j, K)
       Cons(x, tail)
     }
-  } ensuring(res => {
-  	columnLem(j, K) &&
-  	deps(j + 1, K) &&
-  	steps <= ? * ((K - i) * K) + ? * K + ?
+  } ensuring (res => {
+    columnLem(j, K) &&
+      deps(j + 1, K) &&
+      steps <= ? * ((K - i) * K) + ? * K + ?
   })
 
   @invisibleBody
   def fillTable(j: BigInt, T: BigInt, K: BigInt): List[List[BigInt]] = {
+    decreases(abs(T - j))
     require(j >= 0 && T >= j && K >= 0 && deps(j, K))
-    if(j == T) {
+    if (j == T) {
       Cons(fillColumn(0, j, K), Nil[List[BigInt]]())
-    }
-    else {
+    } else {
       val x = fillColumn(0, j, K)
       val tail = fillTable(j + 1, T, K)
       Cons(x, tail)
     }
-  } ensuring(res => deps(T + 1, K) && steps <= ? *((K * K) * (T - j)) + ? * ((T - j)*K) + ? * (T - j) + ? * (K*K) + ? * K + ?)
+  } ensuring (res => deps(T + 1, K) && steps <= ? * ((K * K) * (T - j)) + ? * ((T - j) * K) + ? * (T - j) + ? * (K * K) + ? * K + ?)
 
   def viterbiSols(T: BigInt, K: BigInt): List[List[BigInt]] = {
     require(T >= 0 && K >= 0)
     fillTable(0, T, K)
-  } ensuring(res => deps(T + 1, K) && steps <= ? * ((K * K) * T) + ? * ((T)*K) + ? * T + ? * (K*K) + ? * K + ?)
+  } ensuring (res => deps(T + 1, K) && steps <= ? * ((K * K) * T) + ? * ((T) * K) + ? * T + ? * (K * K) + ? * K + ?)
 
 }
